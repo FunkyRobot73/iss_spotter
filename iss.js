00f3659f974d684d9a5d944aef5229af66b7a2e1
vagrant <arlo@gmail.com>
2021-11-05 01:59:32 +0000

    1st Core Implementation

---
+++ iss.js
@@ -0,0 +1,32 @@
+const request = require('request');
+
+const fetchMyIP = function(callback) { 
+  request("https://api.ipify.org?format=json", (error, response, body) => {
+    if (error) return callback(error, null);
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
+      return;
+
+    }
+
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
+  });
+};
+
+module.exports = { fetchMyIP };
+
+// to be fair I didn't straight up copy n paste...  I typed it out myself to at least try to understand it.
+
+//***************************************************************** */
+// iss.js
+
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
